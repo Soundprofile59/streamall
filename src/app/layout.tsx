21c4a06854d57ad26926b7e8d42e0f5ea1960de9
Soundprofile59 <steeve.soundprofile@gmail.com>
2026-08-26 08:02:05 +0200

    chore: bump Streamall to v0.8.8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ import "./v083-mood-matrix.css";
 import "./v084-album-delete.css";
 import "./build-badge.css";
 
-const APP_VERSION = "0.8.7";
+const APP_VERSION = "0.8.8";
 
 export const metadata: Metadata = {
   title: "Streamall",
